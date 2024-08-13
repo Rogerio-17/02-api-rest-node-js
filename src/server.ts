647a42131a5aed7783ed6d4e1882cbbaa1085677
Rogerio-17 <rogeriojmf10@gmail.com>
2024-08-12 21:48:31 -0300

    feat: adiciona validações de variaveis locais, adiciona readme e cria as tabelas do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
-app.get("/", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+app.get("/transactions", async () => {
+  const transactions = await knex("transactions")
+    .where("amount", 52)
+    .select("*");
 
-  return tables;
+  return transactions;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running");
